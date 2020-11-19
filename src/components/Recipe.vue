6a13aa7dae7e1e258bc054581d6f4a144dad5424
Marcus Sera <m.yoshiosera@gmail.com>
2020-11-18 22:56:14 -0800

    working on iframe content

--- src/components/Recipe.vue
+++ src/components/Recipe.vue
@@ -4,8 +4,12 @@
     <iframe
       src="../assets/img/test.pdf"
       width="100%"
-      height="100%"
       loading="lazy"
+      scrolling="no"
+      allowtransparency="true"
+      :height="iframeHeight"
+      ref="chartiframe"
+      id="datawrapper-chart-8dfPN"
     ></iframe>
     <!-- <pdf src="@/assets/img/test.pdf"></pdf> -->
   </div>
@@ -13,11 +17,62 @@
 
 <script>
 // import pdf from 'vue-pdf';
-
+const DELTAS = {
+  '100': 481,
+  '200': 427,
+  '300': 400,
+  '400': 400,
+  '500': 400,
+  '700': 400,
+  '800': 400,
+  '900': 400,
+  '1000': 400,
+};
 export default {
   name: 'Recipe',
+  data() {
+    return {
+      iframeHeight: '0px',
+    };
+  },
+  computed: {
+    // Necessary for initial iframe height
+    iframeOffsetWidth() {
+      return this.$refs['chartiframe'].offsetWidth;
+    },
+  },
+  mounted() {
+    // set initial iframe height
+    this.iframeHeight = `${
+      DELTAS[
+        Math.min(
+          1e3,
+          Math.max(100 * Math.floor(this.iframeOffsetWidth / 100), 100)
+        )
+      ]
+    }px`;
+    // setup event listener
+    window.addEventListener('message', this.handleIframeResize);
+  },
+  beforeUnmount() {
+    // destroy event listener
+    window.removeEventListener('message', this.handleIframeResize);
+  },
+  methods: {
+    handleIframeResize(e) {
+      for (var b in e.data['datawrapper-height']) {
+        if ('8dfPN' === b) {
+          this.iframeHeight = `${e.data['datawrapper-height'][b]}px`;
+        }
+      }
+    },
+  },
   // components: { pdf },
 };
 </script>
 
-<style></style>
+<style scoped>
+iframe {
+  border: 0;
+}
+</style>
