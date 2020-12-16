9c9f24354135bbb6c5f732bd9a1d2e6e08242201
MarcusYSera <m.yoshiosera@gmail.com>
2020-12-15 23:41:17 -0800

    working with custom inputs and their form bindings

--- client/src/components/schedule/AddEvent.vue
+++ client/src/components/schedule/AddEvent.vue
@@ -3,13 +3,11 @@
     <img src="@/assets/svg/addCircle.svg" />
   </button>
   <aside v-if="openAddEvent" class="add-container">
-    <input type="text" placeholder=" Add Recipe Name" />
-    <input
-      type="date"
-      :value="getCurrentDate(currentDate)"
-      :min="getCurrentDate(currentDate)"
-    />
-    <input type="time" :value="getCurrentTime(currentDate)" />
+    <input type="text" placeholder=" Add Recipe Name" v-model="recipeName" />
+    <input type="date" :value="currentDate" :min="currentDate" />
+    {{ currentDate }}
+    <input type="time" v-model="currentTime" />
+    {{ currentTime }}
     <button type="submit">Create</button>
   </aside>
 </template>
@@ -20,33 +18,36 @@ export default {
   data() {
     return {
       openAddEvent: false,
-      currentDate: new Date(),
+      recipeName: '',
     };
+  },
+  computed: {
+    currentTime: {
+      get: () => {
+        let newTime = new Date()
+          .toLocaleTimeString(undefined, { hour12: false })
+          .split(':');
+        newTime.pop();
+        return newTime.join(':');
+      },
+      set: newValue => {
+        return newValue;
+      },
+    },
+    currentDate: () => {
+      let t = new Date().toLocaleDateString().split('/');
+      let holder = t.pop();
+      t.unshift(holder);
+      console.log(holder);
+      console.log(t.join('-'));
+      return t.join('-');
+    },
   },
   methods: {
     openEvent() {
       this.openAddEvent = !this.openAddEvent;
     },
-    getCurrentDate(date) {
-      return date.toISOString().split('T')[0];
-    },
-    getCurrentTime(date) {
-      let newTime = date
-        .toLocaleTimeString(undefined, { hour12: false })
-        .split(':');
-      newTime.pop();
-      return newTime.join(':');
-      // return date.toLocaleTimeString(undefined, { hour12: false });
-    },
   },
-  // created() {
-  //   let [month, date, year] = new Date()
-  //     .toLocaleDateString(undefined)
-  //     .split('/');
-  //   this.currentDate = `${year}-${month}-${date}`;
-  //   console.log(this.currentDate);
-  //   return this.currentDate;
-  // },
 };
 </script>
 
