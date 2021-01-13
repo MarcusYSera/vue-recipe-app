36bc0875a573a76d3118e2a372da9a640602c460
MarcusYSera <m.yoshiosera@gmail.com>
2021-01-12 23:52:52 -0800

    adding recipe and event controller

---
+++ express-postgres/src/controllers/recipe.js
@@ -0,0 +1,32 @@
+import Model from '../models/model';
+
+const recipeModel = new Model('recipes');
+
+export const recipesPage = async (req, res) => {
+  try {
+    const data = await recipiesModel.select(
+      'private',
+      'recipe_name',
+      'recipe_image',
+      'cuisine',
+      'duration',
+      'description',
+      'steps'
+    );
+    res.status(200).json({ recipes: data.rows });
+  } catch (err) {
+    res.status(200).json({ recipes: err.stack });
+  }
+};
+
+export const addRecipe = async (req, res) => {
+  const { private, recipeName, recipeImage, cuisine, duration, description, steps } = req.body;
+  const columns = 'private, recipe_name, recipe_image, cuisine, duration, description, steps';
+  const values = `'${private}', '${recipeName}', '${recipeImage}', '${cuisine}', '${duration}', '${description}', ''${steps}`;
+  try {
+    const data = await recipesModel.insertWithReturn(columns, values);
+    res.status(200).json({ recipes: data.rows });
+  } catch (err) {
+    res.status(200).json({ recipes: err.stack });
+  }
+};
