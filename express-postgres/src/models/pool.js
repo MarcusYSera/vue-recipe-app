5d8c94c23656461043b3405e5f1fe5a240db1535
MarcusYSera <m.yoshiosera@gmail.com>
2021-01-14 22:26:31 -0800

    adding local connection

--- express-postgres/src/models/pool.js
+++ express-postgres/src/models/pool.js
@@ -4,4 +4,12 @@ import { connectionString } from '../settings';
 
 dotenv.config();
 
-export const pool = new Pool({ connectionString });
+// export const pool = new Pool({ connectionString });
+
+export const pool = new Pool({
+  user: 'me',
+  host: 'localhost',
+  database: 'vue_recipe_app',
+  password: 'password',
+  port: 5432,
+});
