526cccf3697a03abb4fbc9b069209876999e2a67
MarcusYSera <m.yoshiosera@gmail.com>
2021-01-15 21:27:55 -0800

    adding addEvent

--- express-postgres/src/controllers/event.js
+++ express-postgres/src/controllers/event.js
@@ -10,3 +10,20 @@ export const eventPage = async (req, res) => {
     res.status(200).json({ events: err.stack });
   }
 };
+
+// export const findAllEventsForUserById = async (req, res) => {
+//   const {id} = req.params;
+// };
+
+export const addEventByUserId = async (req, res) => {
+  const { userId } = req.params;
+  const { eventName, eventTime, eventDate } = req.body;
+  const columns = 'event_name, event_time, event_date';
+  const values = `'${eventName}', '${eventTime}', '${eventDate}'`;
+  try {
+    const data = await eventModel.insertWithReturn(columns, values);
+    res.status(200).json({ events: data.rows });
+  } catch (err) {
+    res.status(200).json({ events: err.stack });
+  }
+};
