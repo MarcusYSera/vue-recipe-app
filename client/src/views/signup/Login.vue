14e60b54e4e66b180d1f6abfdb936bc5e3437db7
MarcusYSera <m.yoshiosera@gmail.com>
2021-01-21 23:38:33 -0800

    signup and login semantic html done

--- client/src/views/signup/Login.vue
+++ client/src/views/signup/Login.vue
@@ -1,5 +1,18 @@
 <template>
-  <article>Login Page</article>
+  <article>
+    <section>
+      <h3>Login</h3>
+      <input type="text" placeholder="Email" />
+      <input type="password" placeholder="Password" />
+      <button type="submit">Login</button>
+    </section>
+    <section>
+      <router-link to="/signup" tag="button">Create Account</router-link>
+    </section>
+    <nav>
+      <p></p>
+    </nav>
+  </article>
 </template>
 
 <script>
--- client/src/views/signup/SignUp.vue
+++ client/src/views/signup/SignUp.vue
@@ -1,5 +1,21 @@
 <template>
-  <article>Sign Up Page</article>
+  <article>
+    <h3>Sign Up</h3>
+    <form>
+      <input type="text" placeholder="First Name" />
+      <input type="text" placeholder="Last Name" />
+      <input type="text" placeholder="Email" />
+      <input type="password" placeholder="Password" />
+      <input type="password" placeholder="Confirm Password" />
+      <button type="submit">Sign Up</button>
+    </form>
+    <nav>
+      <p>
+        Already have an account?
+        <router-link to="/login">Login</router-link>
+      </p>
+    </nav>
+  </article>
 </template>
 
 <script>
