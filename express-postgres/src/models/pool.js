f7e11525ecf537587bcd1324f984839a45cdd012
MarcusYSera <m.yoshiosera@gmail.com>
2021-02-02 23:17:21 -0800

    trying travis db postgres user

--- express-postgres/src/models/pool.js
+++ express-postgres/src/models/pool.js
@@ -3,16 +3,25 @@ import pkg from 'pg';
 import dotenv from 'dotenv';
 
 const { Pool } = pkg;
-// import { connectionString } from '../settings';
 
 dotenv.config();
 
+// elephantsql database
+// import { connectionString } from '../settings';
 // export const pool = new Pool({ connectionString });
 
+// local instance with user me
+// export const pool = new Pool({
+//   user: 'me',
+//   host: 'localhost',
+//   database: 'vue_recipe_app',
+//   password: 'password',
+//   port: 5432,
+// });
+
+// travis ci test
 export const pool = new Pool({
-  user: 'me',
-  host: 'localhost',
-  database: 'vue_recipe_app',
-  password: 'password',
+  user: 'postgres',
+  database: 'testdb',
   port: 5432,
 });
