155cdbe60650183ad0e1e712e20d02e743f37a77
MarcusYSera <m.yoshiosera@gmail.com>
2021-02-02 23:52:10 -0800

    travis

--- express-postgres/src/models/pool.js
+++ express-postgres/src/models/pool.js
@@ -7,8 +7,8 @@ const { Pool } = pkg;
 dotenv.config();
 
 // elephantsql database
-// import { connectionString } from '../settings';
-// export const pool = new Pool({ connectionString });
+import { connectionString } from '../settings';
+export const pool = new Pool({ connectionString });
 
 // local instance with user me
 // export const pool = new Pool({
@@ -18,11 +18,3 @@ dotenv.config();
 //   password: 'password',
 //   port: 5432,
 // });
-
-// travis ci test
-export const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'testdb',
-  port: 5432,
-});
