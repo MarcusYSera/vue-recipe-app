22ca959d6b9ee5f8dca4c12250cabe04bcb98d55
MarcusYSera <m.yoshiosera@gmail.com>
2021-02-02 23:43:15 -0800

    trouble shooting travis

--- express-postgres/test/hooks.js
+++ express-postgres/test/hooks.js
@@ -7,18 +7,22 @@ import {
   addForeignKeys,
   insertPrimaryIntoTables,
   insertForeignIntoTables,
+  createTriggerFunctions,
+  associateUpdateTriggerToTables,
 } from '../src/utils/queryFunctions.js';
 
 before(async () => {
+  await createTriggerFunctions();
   await createPrimaryKeyTables();
   await createForeignKeyTables();
   await addForeignKeys();
+  await associateUpdateTriggerToTables();
   await insertPrimaryIntoTables();
   await insertForeignIntoTables();
 });
 
 after(async () => {
-  await deleteTestValues();
-  //   await dropForeignKeyTables();
-  //   await dropPrimaryKeyTables();
+  // await deleteTestValues();
+  await dropForeignKeyTables();
+  await dropPrimaryKeyTables();
 });
