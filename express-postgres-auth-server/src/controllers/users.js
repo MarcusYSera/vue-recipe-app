2952f9357ee7d0d5915202e25638cfb18af5eb63
MarcusYSera <m.yoshiosera@gmail.com>
2021-03-03 15:01:43 -0800

    auth server with jwt working

--- express-postgres-auth-server/src/controllers/users.js
+++ express-postgres-auth-server/src/controllers/users.js
@@ -6,12 +6,20 @@ import jwt from 'jsonwebtoken';
 
 import { jwtAccessTokenSecret, jwtRefreshTokenSecret } from './../settings.js';
 
+let refreshTokens = [];
+
 export const loginUser = async (req, res) => {
   const email = req.body.email;
   const user = { email: email };
 
-  const jwtAccessToken = jwt.sign(user, jwtAccessTokenSecret);
-  res.json({ jwtAccessToken: jwtAccessToken });
+  const jwtAccessToken = generateAccessToken(user);
+  const jwtRefreshToken = jwt.sign(user, jwtRefreshTokenSecret);
+  refreshTokens.push(jwtRefreshToken);
+  res.json({ jwtAccessToken: jwtAccessToken, jwtRefreshToken: jwtRefreshToken });
+};
+
+const generateAccessToken = (user) => {
+  return jwt.sign(user, jwtAccessTokenSecret, { expiresIn: '15s' });
 };
 
 export const usersPage = async (req, res) => {
@@ -21,4 +29,20 @@ export const usersPage = async (req, res) => {
   } catch (err) {
     res.status(200).json({ users: err.stack });
   }
+};
+
+export const getJWTRefreshToken = async (req, res) => {
+  const refreshToken = req.body.jwtRefreshToken;
+  if (refreshToken == null) return res.sendStatus(401);
+  if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+  jwt.verify(refreshToken, jwtRefreshTokenSecret, (err, user) => {
+    if (err) return res.sendStatus(403);
+    const accessToken = generateAccessToken({ email: user.email });
+    res.json({ accessToken: accessToken });
+  });
+};
+
+export const logoutUser = async (req, res) => {
+  refreshTokens = refreshTokens.filter((token) => token !== req.body.jwtRefreshToken);
+  res.sendStatus(204);
 };
--- express-postgres-auth-server/src/routes/index.js
+++ express-postgres-auth-server/src/routes/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { loginUser, usersPage } from './../controllers/index.js';
+import { loginUser, usersPage, getJWTRefreshToken, logoutUser } from './../controllers/index.js';
 import { authenticateToken } from './../middleware/index.js';
 
 const indexRouter = express.Router();
 
 indexRouter.post('/users/login', loginUser);
+indexRouter.post('/users/login/refreshtoken', getJWTRefreshToken);
 indexRouter.get('/users', authenticateToken, usersPage);
+indexRouter.delete('/users/logout', logoutUser);
 
 export default indexRouter;
