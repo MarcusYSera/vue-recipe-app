db254cf47cd657ea46b87788a94bc36e51f88255
MarcusYSera <m.yoshiosera@gmail.com>
2021-03-10 19:20:35 -0800

    adjusted route names

--- express-postgres-authorization/src/routes/index.js
+++ express-postgres-authorization/src/routes/index.js
@@ -14,10 +14,10 @@ const authorizationRouter = express.Router();
 authorizationRouter.get('/users', checkForCookie, authenticateToken, usersPage);
 
 // authorizationRouter.post('users/signup', createUser);
-authorizationRouter.post('/users/login', loginReturnAuthorizationToken);
+authorizationRouter.post('/users/auth/login', loginReturnAuthorizationToken);
 
 // authorizationRouter.post('/users/authorization', createAuthorizationToken); //  login, create access token and refresh token
-authorizationRouter.post('/users/login/refreshauthtoken', getJWTRefreshAuthToken); //  silent refresh, create new access token, if expired, using refresh token
-authorizationRouter.delete('/users/logout', logoutUser); //  invalidate access token
+authorizationRouter.post('/users/auth/refreshauthtoken', getJWTRefreshAuthToken); //  silent refresh, create new access token, if expired, using refresh token
+authorizationRouter.delete('/users/auth/logout', logoutUser); //  invalidate access token
 
 export default authorizationRouter;
