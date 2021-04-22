191d2ebad12c90e2f74e649e8d79b872cfb985ac
MarcusYSera <m.yoshiosera@gmail.com>
2021-04-21 21:39:20 -0700

    adding g analytics

--- client/src/components/recipe/RecipePreview.vue
+++ client/src/components/recipe/RecipePreview.vue
@@ -24,6 +24,9 @@ export default {
   methods: {},
   mounted() {
     console.log(this.rawRecipe);
+    console.log(typeof this.rawRecipe);
+    const indexOfFirst = this.rawRecipe.indexOf('Yield');
+    console.log(indexOfFirst);
   },
 };
 </script>
