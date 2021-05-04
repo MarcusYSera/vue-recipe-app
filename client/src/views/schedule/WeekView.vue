2a3c96de7359437434526386bc8cfa0c2b7e1bad
MarcusYSera <m.yoshiosera@gmail.com>
2021-05-03 22:27:14 -0700

    starting week view for schedule display

---
+++ client/src/views/schedule/WeekView.vue
@@ -0,0 +1,14 @@
+<template>
+  <section class="grid-container weekview"></section>
+</template>
+
+<script>
+export default {
+  name: 'WeekView',
+};
+</script>
+
+<style scoped>
+/* .weekview {
+} */
+</style>
