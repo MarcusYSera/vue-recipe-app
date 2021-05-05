37ebe2e6585706142d8867dd4d11505c5ad28312
MarcusYSera <m.yoshiosera@gmail.com>
2021-05-04 23:29:44 -0700

    finished hour data

--- client/src/components/data/TwentyFourHourTimeByHour.js
+++ client/src/components/data/TwentyFourHourTimeByHour.js
@@ -1,5 +1,13 @@
 const twentyFourHoursByHour = () => {
   let time = ['00:00'];
+  let current = '00:00';
+  for (let i = 1; i < 25; i++) {
+    let hour = current.split(':')[0];
+    if (i < 10) hour = `0${i}`;
+    if (i > 9) hour = `${i}`;
+    time.push(`${hour}:00`);
+  }
+  return time;
 };
 
 const twentyFourHoursByHourData = twentyFourHoursByHour();
