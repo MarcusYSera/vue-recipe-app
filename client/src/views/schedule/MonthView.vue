e51df3d4217e62167aeb6f4100a5d8f161ed1e22
MarcusYSera <m.yoshiosera@gmail.com>
2021-05-04 23:33:56 -0700

    update timebar to accept different values

--- client/src/views/schedule/MonthView.vue
+++ client/src/views/schedule/MonthView.vue
@@ -1,10 +1,16 @@
 <template>
-  <article class="grid-container monthview">Month View</article>
+  <article class="grid-container monthview">
+    MonthView
+    <!-- <TimeBar></TimeBar> -->
+  </article>
 </template>
 
 <script>
+// import TimeBar from '@/components/schedule/TimeBar';
+
 export default {
   name: 'MonthView',
+  // components: { TimeBar },
 };
 </script>
 
--- client/src/views/schedule/WeekView.vue
+++ client/src/views/schedule/WeekView.vue
@@ -1,10 +1,15 @@
 <template>
-  <article class="grid-container weekview">Week View</article>
+  <article class="grid-container weekview">
+    <TimeBar></TimeBar>
+  </article>
 </template>
 
 <script>
+import TimeBar from '@/components/schedule/TimeBar';
+
 export default {
   name: 'WeekView',
+  components: { TimeBar },
 };
 </script>
 
